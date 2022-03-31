d00b1b64898a8f9bf3885d21ffd12f89cc634926
melatirisni <102685492+melatirisni@users.noreply.github.com>
2022-03-31 12:23:10 +0700

    Update locks.py

--- EmikoRobot/modules/locks.py
+++ EmikoRobot/modules/locks.py
@@ -541,20 +541,20 @@ You're in the right place!
 The locks module allows you to lock away some common items in the \
 telegram world; the bot will automatically delete them!
 
-❂ /locktypes*:* Lists all possible locktypes
+❂ /locktypes *:* Lists all possible locktypes
 
-*Admins only:*
-❂ /lock <type>*:* Lock items of a certain type (not available in private)
-❂ /unlock <type>*:* Unlock items of a certain type (not available in private)
-❂ /locks*:* The current list of locks in this chat.
+*Admins only :*
+❂ /lock <type> *:* Lock items of a certain type (not available in private)
+❂ /unlock <type> *:* Unlock items of a certain type (not available in private)
+❂ /locks *:* The current list of locks in this chat.
 
 Locks can be used to restrict a group's users.
-eg:
+eg :
 Locking urls will auto-delete all messages with urls, locking stickers will restrict all \
 non-admin users from sending stickers, etc.
 Locking bots will stop non-admins from adding bots to the chat.
 
-*Note:*
+*Note :*
 ❂ Unlocking permission *info* will allow members (non-admins) to change the group information, such as the description or the group name
 ❂ Unlocking permission *pin* will allow members (non-admins) to pinned a message in a group
 """
