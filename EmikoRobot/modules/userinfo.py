3742107abadb8f87cf8c27fe0db5a4b05987d4fa
melatirisni <102685492+melatirisni@users.noreply.github.com>
2022-03-31 17:08:12 +0700

    Update userinfo.py

--- EmikoRobot/modules/userinfo.py
+++ EmikoRobot/modules/userinfo.py
@@ -563,13 +563,13 @@ Examples:
 *json Detailed info:*
 ❂ /json *:* Get Detailed info about any message.
  
-*AFk:*
+*AFk :*
 When marked as AFK, any mentions will be replied to with a message stating that you're not available!
 ❂ /afk <reason> *:* Mark yourself as AFK.
   - brb <reason> : Same as the afk command, but not a command. 
   
 *What is that health thingy?*
- Come and see [HP System explained](https://telegra.ph/hp-system-explained-03-31)
+Come and see [HP System explained](https://telegra.ph/hp-system-explained-03-31)
 """
 
 SET_BIO_HANDLER = DisableAbleCommandHandler("setbio", set_about_bio, run_async=True)
