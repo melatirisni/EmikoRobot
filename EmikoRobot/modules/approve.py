13324636723c9505a414d6c81d64ac8940d140fa
melatirisni <102685492+melatirisni@users.noreply.github.com>
2022-03-31 20:15:54 +0700

    Update approve.py

--- EmikoRobot/modules/approve.py
+++ EmikoRobot/modules/approve.py
@@ -196,7 +196,7 @@ __help__ = """
 Sometimes, you might trust a user not to send unwanted content.
 Maybe not enough to make them admin, but you might be ok with locks, blacklists, and antiflood not applying to them.
 
-That's what approvals are for - approve of trustworthy users to allow them to send
+That's what approvals are for - approve of trustworthy users to allow them to send.
 
 *Admin commands:*
 ❂ /approval *:* Check a user's approval status in this chat.
