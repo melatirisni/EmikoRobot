25f7c0d788a935e08aec526df6afd1af4e98143f
melatirisni <102685492+melatirisni@users.noreply.github.com>
2022-03-31 10:32:38 +0700

    Update tagall.py

--- EmikoRobot/modules/tagall.py
+++ EmikoRobot/modules/tagall.py
@@ -109,7 +109,7 @@ async def cancel_spam(event):
         return await event.respond("__Stopped Mention.__")
 
 
-__mod_name__ = "Tag all"
+__mod_name__ = "Tag All"
 __help__ = """
 ──「 Mention all func 」──
 
