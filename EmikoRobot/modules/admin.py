6fe2f7c4756709b56d21792cbd0afda48d38f31e
melatirisni <102685492+melatirisni@users.noreply.github.com>
2022-03-31 20:07:24 +0700

    Update admin.py

--- EmikoRobot/modules/admin.py
+++ EmikoRobot/modules/admin.py
@@ -922,9 +922,9 @@ def button(update: Update, context: CallbackContext) -> str:
 __help__ = """
 *User Commands*:
 ❂ /admins *:* list of admins in the chat
-❂ /pinned *:* to get the current pinned message.
+❂ /pinned *:* to get the current pinned message
 
-*The Following Commands are Admins only:* 
+*The Following Commands are Admins only :* 
 ❂ /pin *:* silently pins the message replied to - add `'loud'` or `'notify'` to give notifs to users
 ❂ /unpin *:* unpins the currently pinned message
 ❂ /invitelink *:* gets invitelink
@@ -934,7 +934,7 @@ __help__ = """
 ❂ /title <title here> *:* sets a custom title for an admin that the bot promoted
 ❂ /admincache *:* force refresh the admins list
 ❂ /del *:* deletes the message you replied to
-❂ /purge *:* deletes all messages between this and the replied to message.
+❂ /purge *:* deletes all messages between this and the replied to message
 ❂ /purge <integer X> *:* deletes the replied message, and X messages following it if replied to a message.
 ❂ /setgtitle <text> *:* set group title
 ❂ /setgpic *:* reply to an image to set as group photo
@@ -942,9 +942,9 @@ __help__ = """
 ❂ /setsticker *:* Set group sticker
 
 *Rules*:
-❂ /rules *:* get the rules for this chat.
-❂ /setrules <your rules here> *:* set the rules for this chat.
-❂ /clearrules *:* clear the rules for this chat.
+❂ /rules *:* get the rules for this chat
+❂ /setrules <your rules here> *:* set the rules for this chat
+❂ /clearrules *:* clear the rules for this chat
 """
 
 SET_DESC_HANDLER = CommandHandler("setdesc", set_desc, filters=Filters.chat_type.groups, run_async=True)
