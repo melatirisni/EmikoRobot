144deca2d1b25396bb27c250a8f4eddf83b6f707
melatirisni <102685492+melatirisni@users.noreply.github.com>
2022-03-31 20:55:51 +0700

    Update musichelp.py

--- EmikoRobot/modules/musichelp.py
+++ EmikoRobot/modules/musichelp.py
@@ -1,8 +1,8 @@
 __mod_name__ = "Music/Video"
 
 __help__ = """
-Available Commands : play , vplay , cplay
-ForcePlay Commands : playforce , vplayforce , cplayforce
+Available Commands : play, vplay, cplay.
+ForcePlay Commands : playforce, vplayforce, cplayforce.
 
 c stands for channel play.
 v stands for video play.
@@ -12,25 +12,25 @@ force stands for force play.
 ❂ /playforce or /vplayforce or /cplayforce -  Force Play stops the current playing track on voice chat and starts playing the searched track instantly without disturbing/clearing queue.
 ❂ /channelplay [Chat username or id] or [Disable] - Connect channel to a group and stream music on channel's voice chat from your group.
 ❂ /playlist - Check Your Saved Playlist On Servers.
-❂ /deleteplaylist - Delete any saved music in your playlist
-❂ /play  - Start playing Your Saved Playlist from Servers.
+❂ /deleteplaylist - Delete any saved music in your playlist.
+❂ /play - Start playing Your Saved Playlist from Servers.
 
 • Admins only
-c stands for channel play.
-❂ /settings - set menu, auth or lang
+❂ c stands for channel play.
+❂ /settings - set menu, auth or lang.
 ❂ /pause or /cpause - Pause the playing music.
 ❂ /resume or /cresume- Resume the paused music.
-❂ /mute or /cmute- Mute the playing music
-❂ /unmute or /cunmute- Unmute the muted music
+❂ /mute or /cmute- Mute the playing music.
+❂ /unmute or /cunmute- Unmute the muted music.
 ❂ /shuffle or /cshuffle- Randomly shuffles the queued playlist.
-❂ /seek or /cseek - Forward Seek the music to your duration
-❂ /seekback or /cseekback - Backward Seek the music to your duration
+❂ /seek or /cseek - Forward Seek the music to your duration.
+❂ /seekback or /cseekback - Backward Seek the music to your duration.
 ❂ /restart - Restart bot for your chat .
 
 • Auth User
 
-❂ /auth [Username] - Add a user to AUTH LIST of the group.
-❂ /unauth [Username] - Remove a user from AUTH LIST of the group.
+❂ /auth [username] - Add a user to AUTH LIST of the group.
+❂ /unauth [username] - Remove a user from auth list of the group.
 ❂ /authusers - Check AUTH LIST of the group.
 
 """
